6e66534d22ddf08431215df741bec47e3c08199b
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2025-08-12 21:05:48 +0530

    Time: 8 ms (98.47%) | Memory: 13.7 MB (64.71%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,22 @@
+static const int MOD = 1'000'000'007;
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        vector<int> powers;
+        for (int i = 1; ; ++i) {
+            long long p = 1;
+            for (int k = 0; k < x; ++k) p *= i;
+            if (p > n) break;
+            powers.push_back((int)p);
+        }
+
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int p : powers) {
+            for (int s = n; s >= p; --s) {
+                dp[s] = (dp[s] + dp[s - p]) % MOD;
+            }
+        }
+        return (int)dp[n];
+    }
+};
