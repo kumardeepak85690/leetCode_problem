d294cc480e32cf6edcdb4c415655902e4107180e
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2025-11-28 22:27:48 +0530

    Time: 154 ms (41.28%) | Memory: 191.1 MB (17.79%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
-    int ans=0;
-    vector<vector<int>> adj;
-    bitset<30001> visited=0;
-    inline void build_adj(int n, vector<vector<int>>& edges, vector<int>& values)
-    {
-        for(auto& e: edges){
-            int i=e[0], j=e[1];
-            adj[i].push_back(j);
-            adj[j].push_back(i);
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int> > G(n);
+        for(auto edge : edges){
+            G[edge[0]].push_back(edge[1]);
+            G[edge[1]].push_back(edge[0]);
         }
-    }
-    inline long long dfs(int i, vector<int>& values, int k){
-        visited[i]=1;
-        long long sum=values[i];
-        for (int j: adj[i]){
-            if (visited[j]) continue;
-            sum+=dfs(j, values, k);
-            sum%=k;
+        stack <int> S;
+        vector<int> parent(n);
+        
+        auto DFS = [&](int u, int p, auto &&DFS) -> void{
+            parent[u] = p;
+            S.push(u);
+            for(auto v : G[u]){
+                if(v == p) continue;
+                DFS(v, u, DFS);
+            }
+        };
+        DFS(0, -1, DFS);
+        vector<long long> sum(n);
+        for(int i = 0; i < n; i ++){
+            sum[i] = values[i];
         }
-        if (sum%k==0){
-            ans++;
-            return 0;
+        int ans = 0;
+        while(!S.empty()){
+            int u = S.top();
+            S.pop();
+            if(sum[u] % k == 0){
+                ans += 1;
+            }
+            else{
+                sum[parent[u]] += sum[u];
+            }
         }
-        return sum;
-    }
-    
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
-    {
-        adj.resize(n);
-    
-        build_adj(n, edges, values);
-
-        dfs(0, values, k);
-
         return ans;
     }
 };
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
