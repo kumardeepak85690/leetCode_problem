7e6bfa04c37096644b43f8f23be5545724d376f9
DEEPAK KUMAR <78249949+kumardeepak85690@users.noreply.github.com>
2025-12-23 23:25:33 +0530

    Time: 70 ms (76.83%) | Memory: 165.5 MB (54.83%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    int maxTwoEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        
-        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        
-        vector<int> suffixMax(n);
-        suffixMax[n - 1] = events[n - 1][2];
-        
-        for (int i = n - 2; i >= 0; --i) {
-            suffixMax[i] = max(events[i][2], suffixMax[i + 1]);
+    using info=tuple<int, bool, int>;
+    static int maxTwoEvents(vector<vector<int>>& events) {
+        const int n=events.size();
+        vector<info> time(n*2);
+        for(int i=0; i<n; i++){
+            int s=events[i][0], e=events[i][1], v=events[i][2];
+            time[2*i]={s, 0, v};
+            time[2*i+1]={e, 1, v};
         }
-        
-        int maxSum = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            int left = i + 1, right = n - 1;
-            int nextEventIndex = -1;
-            
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (events[mid][0] > events[i][1]) {
-                    nextEventIndex = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            
-            if (nextEventIndex != -1) {
-                maxSum = max(maxSum, events[i][2] + suffixMax[nextEventIndex]);
-            }
-            
-            maxSum = max(maxSum, events[i][2]);
+        sort(time.begin(), time.end());
+        int ans=0, maxV=0, n2=n*2;
+        for(auto& [t, isEnd, v]: time){
+            if (isEnd) maxV=max(maxV, v);
+            else ans=max(ans, maxV+v);
         }
-        
-        return maxSum;
+        return ans;    
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
